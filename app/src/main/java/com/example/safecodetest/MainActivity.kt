9db97ba63ca03231103c69450d24cd5d7f547536
gagandeep-nickelfox <gagandeep@nickelfox.co>
2023-10-09 15:36:52 +0530

    added lots of comments

--- app/src/main/java/com/example/safecodetest/MainActivity.kt
+++ app/src/main/java/com/example/safecodetest/MainActivity.kt
@@ -7,5 +7,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
+//        val a = 0
     }
 }
