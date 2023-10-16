d4fa777f1b16fc13d8bad2301d41ba0c508ab1d5
gagandeep-nickelfox <gagandeep@nickelfox.co>
2023-10-16 13:49:12 +0530

    added warning

--- app/src/main/java/com/example/safecodetest/MainActivity.kt
+++ app/src/main/java/com/example/safecodetest/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val a = "sdf".uppercase(Locale.ROOT)
+        val a = "sdf".toUpperCase()
     }
 }
